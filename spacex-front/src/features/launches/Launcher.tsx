63e9a3c29362ef2622eb4dfda96ee7dfa6e4ba3b
Banty Bahru <bantyder@gmail.com>
2023-05-03 01:11:03 +0200

    Retry and stop gracefully

--- spacex-front/src/features/launches/Launcher.tsx
+++ spacex-front/src/features/launches/Launcher.tsx
@@ -1,15 +1,18 @@
-import { Card, Spin } from 'antd';
+import { Card, Spin, notification } from 'antd';
 import useLaunches from '../../hooks/useLaunches';
 import LaunchTable from './LaunchTable';
 import SelectApi from './SelectApi';
 import SelectData from './SelectData';
+import React from 'react';
 
 
 function Launcher() {
   
-  const{isLoading,data,error} = useLaunches();
+  const{isLoading,data} = useLaunches();
 
-  if(error) return <h1>"An error has occurred: " + error</h1>;
+
+
+  
   return (
     <Card title="Space X Launches"  >
     
